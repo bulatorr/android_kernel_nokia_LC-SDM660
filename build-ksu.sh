27ed13b8bdf6b308a96f1ba9ba0d15323695af5b
bulatorr <84269101+bulatorr@users.noreply.github.com>
2024-02-04 17:11:45 +0300

    ksu stuff

---
+++ build-ksu.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "------------------------------------------------------"
+echo "Kernel Source For LC-SDM660-2019 Models"
+echo "------------------------------------------------------"
+
+path=/home/zbltrr
+
+clear
+export ARCH=arm64
+export CROSS_COMPILE=$path/aarch64-linux-android-4.9/bin/aarch64-linux-android-
+mkdir output
+make -C $(pwd) O=output DDV-ksu_defconfig
+make -j32 -C $(pwd) O=output
